e35d8eb97b1d92d25425342b22ad12d2519bb4f7
Sebastian Gonzalez <s.gonzalez.calquin@gmail.com>
2020-04-12 04:00:44 -0400

    Actualización de secciones Estadisticas y Autocuidado con navegacion

--- app/src/main/java/com/example/cuidate/MainActivity.java
+++ app/src/main/java/com/example/cuidate/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cuidate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout autocuidado = (FrameLayout) findViewById(R.id.autocuidado);
+
+        autocuidado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Autocuidado.class);
+                startActivity(intent);
+            }
+        });
+
+        FrameLayout estadisticas = (FrameLayout) findViewById(R.id.estadisticas);
+
+        estadisticas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Estadisticas.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
